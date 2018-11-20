ee3f3a720e8c3637d6239543036c4cba050f7bb1
gordonodira <go37@njit.edu>
2018-11-19 20:04:17 -0500

    QUESTION PROFILE: Added Question migrate, model, and updated the user seeder to create profiles

--- app/User.php
+++ app/User.php
@@ -45,6 +45,10 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 
     /**
      * The attributes that should be hidden for arrays.
